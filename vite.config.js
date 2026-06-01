41d07f3122a17813c75a1824b5e16781ef4cdf5c
tant2012 <terrencetan2012@gmail.com>
2026-06-02 05:16:51 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-netlify'
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  	kit: {
+		// default options are shown
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
+	}
 });
+
+ 
